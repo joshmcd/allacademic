cf8e5d6339c7824235c27ae0d7e8846408fd3227
josh mcdonald <joshuadmcdonald@gmail.com>
2015-02-26 19:29:03 -0500

    finalizing

--- allacademic_scraper.rb
+++ allacademic_scraper.rb
@@ -1,6 +1,5 @@
 #!/usr/bin/env ruby
 #still to do: 
-	#figure out why the encoding is mangling long dashes
 	#move text logic to its own loop
 	#fix broken "background image" link
 	#iterate through pages
@@ -15,14 +14,26 @@ agent = Mechanize.new
 puts "what's the allacademic link? Right-click/paste."
 url = gets
 page = agent.get(url)
-parsed = page.parser
-a = '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />' #have to add this so it doesn't screw up the encoding
-text = a + parsed.css("html", "body", "center", "table", "tbody", "tr", "td", "table", "tbody", "tr", "td", "table.messages_box", "tbody", "tr.messages_box", "td.messages_box", "table.messages_box_off", "tbody", "tr.messages_box_off", "td.messages_box_off", "table tbody tr td div")[43]
+#default 'http://citation.allacademic.com/meta/p_mla_apa_research_citation/0/7/1/2/0/pages71201/p71201-1.php'
 
+parsed = page.parser
+text = parsed.css("html", "body", "center", "table", "tbody", "tr", "td", "table", "tbody", "tr", "td", "table.messages_box", "tbody", "tr.messages_box", "td.messages_box", "table.messages_box_off", "tbody", "tr.messages_box_off", "td.messages_box_off", "table tbody tr td div")[43]
 css = File.open('css.css', 'w') { |i| i << parsed.css('style') }
 css = File.open('css.css', 'r')
-File.open('C:\Users\jmcdon39\Desktop\test.html', 'w') do |i|
+
+File.open('~/test.html', 'w') do |i|
+  i << '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />' #have to add this so it doesn't screw up the encoding
   css.each_line { |p| i << p }
-  i << text
 end
+
+while page.link_with(:text => "next") == true
+	page = page.link_with(:text => "next").click
+	parsed = page.parser
+	text << parsed.css("html", "body", "center", "table", "tbody", "tr", "td", "table", "tbody", "tr", "td", "table.messages_box", "tbody", "tr.messages_box", "td.messages_box", "table.messages_box_off", "tbody", "tr.messages_box_off", "td.messages_box_off", "table tbody tr td div")[43]
+end
+
+File.open('~/test.html', 'w') do |i|
+	i << text
+end
+
 
